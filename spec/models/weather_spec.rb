53941b1f097faa41fc6a4d3e92c167dc3082c3ab
Tucker <tuckerjamesmayo@gmail.com>
2015-11-20 23:56:36 -0500

    add secret keys, create borders and change colors to weather page

---
+++ spec/models/weather_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+require 'rails_helper'
+describe Weather do
+  describe '.index' do
+    @team_weather = Team.new
+    @team_weather.gps_coords.each do |loc|
+    before :each do
+      FORECAST_API_TOKEN="984d060597304a856e61288ff69900cc"
+    end
+  end
+    it 'should return a forecast for a given latitude, longitude' do
+      forecast = Weather.now
+      gps_coords = {:latitude => 37.8267, :longitude => -122.423}
+              expect(forecast).to_not be_nil
+              expect(forecast.latitude).to eq(37.8267)
+              expect(forecast.longitude).to eq(-122.423)
+            end
+            end
+        end
