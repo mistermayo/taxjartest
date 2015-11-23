a467108c7477b15e0d262ea0e7c7ae19e183421e
Tucker <tuckerjamesmayo@gmail.com>
2015-11-23 13:31:39 -0500

    add partial spec

---
+++ spec/models/team_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Team do
+  describe '.initialize' do
+    it "creates a report for the team" do
+      @reports = get_weather_report
+
+      expect(response).to eq({@reports})
+    end
+  end
