c71222695d6ca1ffd8fac59789343fef69b25636
Tucker <tuckerjamesmayo@gmail.com>
2015-11-23 13:56:47 -0500

    add two more passing specs

--- spec/routing/weather_routing_spec.rb
+++ spec/routing/weather_routing_spec.rb
@@ -2,13 +2,11 @@ require "rails_helper"
 
 RSpec.describe WeatherController, type: :routing do
   describe "routing" do
-
     it "routes to #index" do
-      expect(:get => "/weather").to route_to("weather#index")
+      expect(:get => "http://localhost:3000").to route_to("weather#index")
     end
-
     it "routes to #map" do
-     expect(:get => "/weather/map").to route_to("weather#map")
+     expect(:get => "http://localhost:3000/map").to route_to("weather#map")
    end
  end
 end
